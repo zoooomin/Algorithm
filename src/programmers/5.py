3108e00cfb47b127c756ea7cdf07968d8651e1ae
hansumin <sumin_han@naver.com>
2022-11-10 15:20:16 +0900

    ~22.11.10 study

--- src/programmers/5.py
+++ src/programmers/5.py
@@ -1,27 +1,20 @@
+#회전 배열
 t = int(input())
 for test_case in range(1, t+1):
-    n, k = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(n)]
-    s = 0
+    n = int(input())
+    num = [list(input().split()) for _ in range(n)]
+    ans_board = [[0] * n for _ in range(n)]
     cnt = 0
-    b = []
+    for k in range(3):#90,180,270회전//3번 회전
+        n_num = [[0] * n for _ in range(n)]  # 회전한 배열
+        for i in range(n):
+            board = [b[i] for b in num]
+            n_num[i] = board[::-1]
+            ans_board[cnt][i] = ''.join(n_num[i])
+        cnt += 1
+        num = n_num
+    print(f'#{test_case}')
     for i in range(n):
-        s = 0
-        for j in range(n):
-            if board[i][j] == 1:
-                s += 1
-            else:
-                b.append(s)
-                s = 0
-        b.append(s)
-    for i in range(n):
-        s = 0
-        for j in range(n):
-            if board[j][i] == 1:
-                s += 1
-            else:
-                b.append(s)
-                s = 0
-        b.append(s)
-    cnt += b.count(3)
-    print(f'#{test_case} {cnt}')
+        for j in range(3):
+            print(ans_board[j][i], end=' ')
+        print()
